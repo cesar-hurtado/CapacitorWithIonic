10d3df543fed1e817ee607dda96ec9469d203429
Cesar Hurtado <cesar.hurtado@globant.com>
2021-02-09 12:27:16 -0500

    post-clone

--- App/ios/App/appcenter-post-clone.sh
+++ App/ios/App/appcenter-post-clone.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/cesar-hurtado/CapacitorWithIonic.git
+
+# Example: Install App Center CLI
+cd App
+
 npm install
+
 ionic build
